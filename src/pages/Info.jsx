b307ba0c196386058165c3e1f3358788b4f6af89
Niki2406 <nikiranga1998@gmail.com>
2024-07-12 21:50:16 +0530

    Fixed bug in user authentication

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,47 +1,4 @@
 
-// import React from 'react';
-// import { useTranslation } from 'react-i18next';
-
-// const Info = ({ darkMode }) => {
-//   const { t } = useTranslation('info');
-
-//   const handleDownloadClick = () => {
-//     const resumeUrl = '/Niki Resume FinalB1.pdf';
-//     const anchorElement = document.createElement('a');
-//     anchorElement.href = resumeUrl;
-//     anchorElement.download = 'Niki Resume FinalB1.pdf';
-//     anchorElement.target = '_blank';
-//     document.body.appendChild(anchorElement);
-//     anchorElement.click();
-//     document.body.removeChild(anchorElement);
-//   };
-
-//   return (
-//     <div className={`min-h-screen flex items-center justify-center ${darkMode ? 'bg-indigo-950 text-white' : 'bg-slate-200 text-black'}`}>
-//       <div className="container mx-auto px-4 md:px-8 lg:px-12 py-12">
-//         {/* <h2 className={`text-5xl font-bold mb-8 ${darkMode ? 'text-white' : 'text-black'}`}>
-//           {t('infotitle')}
-//         </h2> */}
-//         <h3 className="text-4xl font-bold text-pink-500 mb-4">Niki</h3>
-//         <p className="text-lg mb-4">
-//           Passionate about creating websites with beautiful interfaces. I have experience in web development with several projects completed.
-//         </p>
-//         <p className="text-lg mb-4">
-//           Master's in Computer Application (2024) <br />
-//           B.Sc. Physical Science with Computer (2020) <br />
-//         </p>
-//         <button
-//           onClick={handleDownloadClick}
-//           className={`bg-pink-500 hover:bg-pink-600  text-white py-2 px-4 rounded-full mb-16 ${darkMode ? 'bg-gray-900' : 'bg-pink-500'} transition-colors duration-300`}
-//         >
-//           Download CV
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Info;
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,38 +1,4 @@
-// import React from 'react';
 
-// const Skills = ({ darkMode }) => {
-//   return (
-//     <div className={`${darkMode ? 'bg-indigo-950 text-white' : 'bg-slate-200 text-gray-800'} p-4 md:p-8 lg:p-12`}>
-//       <div className="container mx-auto px-4 md:px-0 lg:px-20">
-//         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
-//           <div className={`relative rounded-lg text-center transform transition-transform hover:scale-105 border-8 border-pink-500 ${darkMode ? 'bg-blue-950' : 'bg-slate-300'} transition-colors duration-300`} style={{ width: '300px', height: '300px' }}>
-//             <div className="flex flex-col justify-center h-full">
-              
-//               <h4 className="text-3xl font-bold mb-2">Front-End</h4>
-//               <p className='text-xl'>HTML, CSS, TailwindCSS, JavaScript, ReactJS, C++, JSON, Redux Toolkit</p>
-//             </div>
-//           </div>
-//           <div className={`relative rounded-lg text-center transform transition-transform hover:scale-105 border-8 border-pink-500 ${darkMode ? 'bg-blue-950' : 'bg-slate-300'} transition-colors duration-300`} style={{ width: '300px', height: '300px' }}>
-//             <div className="flex flex-col justify-center h-full">
-              
-//               <h4 className="text-3xl font-bold mb-2">Tools</h4>
-//               <p className='text-xl'>Visual Studio Code, GitHub, Git, Postman</p>
-//             </div>
-//           </div>
-//           <div className={`relative rounded-lg text-center transform transition-transform hover:scale-105 border-8 border-pink-500 ${darkMode ? 'bg-blue-950' : 'bg-slate-300'} transition-colors duration-300`} style={{ width: '300px', height: '300px' }}>
-//             <div className="flex flex-col justify-center h-full">
-              
-//               <h4 className="text-3xl font-bold mb-2">Back-end</h4>
-//               <p className='text-xl'>Nodejs, MongoDB</p>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Skills;
 import React from 'react';
 
 const Skills = ({ darkMode }) => {
